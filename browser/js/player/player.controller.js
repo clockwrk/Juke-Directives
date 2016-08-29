94c88882b600d17e329a7bf98b3f7ba4df1eaff5
Bruno Raposo <braposo@g.harvard.edu>
2016-08-29 15:23:19 -0400

    end day

--- browser/js/player/player.controller.js
+++ browser/js/player/player.controller.js
@@ -1,21 +1,26 @@
 /* global juke */
 'use strict';
 
-juke.controller('PlayerCtrl', function ($scope, $rootScope, PlayerFactory) {
 
-  angular.extend($scope, PlayerFactory); // copy props from param2 to param1
 
-  $scope.toggle = function () {
+juke.directive('playerDirective', function(PlayerFactory){
+  return {
+    restrict: "E",
+    templateUrl: 'audioplayer.html',
+    link: function(scope, element , attrs){
+      angular.extend(scope, PlayerFactory); 
+  scope.toggle = function () {
     if ( PlayerFactory.isPlaying() ) PlayerFactory.pause();
     else PlayerFactory.resume();
   };
 
-  $scope.getPercent = function () {
+  scope.getPercent = function () {
     return PlayerFactory.getProgress() * 100;
   };
 
-  $scope.handleProgressClick = function (evt) {
+  scope.handleProgressClick = function (evt) {
     PlayerFactory.seek(evt.offsetX / evt.currentTarget.scrollWidth);
   };
-
-});
+    }
+  }
+})
